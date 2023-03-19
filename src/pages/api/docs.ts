f667d814099f1637f23d4ae4ba5ef23132618000
Crystal Cheong <65748007+crystalcheong@users.noreply.github.com>
2023-03-19 15:41:10 +0800

    docs: production exposure

--- src/pages/api/docs.ts
+++ src/pages/api/docs.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { renderTrpcPanel } from "trpc-panel";
 
-import { env } from "@/env.mjs";
+import { getBaseUrl } from "@/utils/api";
 
 import { appRouter } from "../../server/api/root";
 
@@ -9,7 +9,7 @@ import { appRouter } from "../../server/api/root";
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
   res.status(200).send(
     renderTrpcPanel(appRouter, {
-      url: `${env.NEXTAUTH_URL}/api/trpc`,
+      url: `${getBaseUrl()}/api/trpc`,
       transformer: "superjson",
     })
   );
